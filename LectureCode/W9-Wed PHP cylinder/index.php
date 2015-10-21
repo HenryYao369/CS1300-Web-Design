8b23bd2011933b87fdca03fe80ba430ee82e6384
HenryYAO <hengzhi369@gmail.com>
2015-10-21 12:24:25 -0400

    W9-Wed Lecture Code init commit

---
+++ LectureCode/W9-Wed PHP cylinder/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Simple Web Form Example</title>
+    <link rel="stylesheet" type="text/css" href="css/webforms.css">
+</head>
+
+<body>
+
+    <?php include "components/banner.php"; ?>
+
+    <div id="container">
+        <p>The most fun you can have without eating chocolate!</p>
+        <form id="cylinderForm" method="POST" action="results.php">
+            <img src="images/cylinder_xparent.png" alt="cylinder">
+            <p>Enter values for the radius and height of the cylinder:</p>
+            <p><input name="radius" type="text"><label>Radius</label></p>
+            <p><input name="height" type="text"><label>Height</label></p>
+            <input type="submit" value="Submit">
+            <input type="reset" value="Reset">
+        </form>
+        <p class="credit">Cylinder image adapted from <a href="https://en.wikipedia.org/wiki/Cylinder_%28geometry%29">Wikimedia</a>.</p>
+    </div> <!-- end of container div -->
+    
+</body>
+
+</html>
---
+++ LectureCode/W9-Wed PHP cylinder/results.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Simple Web Form Example</title>
+    <link rel="stylesheet" type="text/css" href="css/webforms.css">
+</head>
+
+<body>
+
+    <?php include "components/banner.php"; ?>
+    <?php include "scripts/cylinderCalc.php"; ?>
+
+    <div id="container">
+        <p class="credit">Cylinder image adapted from <a href="https://en.wikipedia.org/wiki/Cylinder_%28geometry%29">Wikimedia</a>.</p>
+        
+        <div id="results">
+            <img src="images/cylinder_xparent.png" alt="cylinder">
+            <h3>For radius <?php echo $radius; ?> and height <?php echo $height; ?>:</h3>
+            <ul>
+                <li>Volume: <?php echo $resultsArray[0]; ?></li>
+                <li>Surface Area: <?php echo $resultsArray[1]; ?></li>
+            </ul>
+        </div> <!-- end of results div -->
+        
+        
+        
+    </div> <!-- end of container div -->
+    
+</body>
+
+</html>
